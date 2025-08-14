4839ed947284813e3eb7de25f402e78ec5c69b79
TwinkleMedia <hs0052264@gmail.com>
2025-08-14 17:47:22 +0530

    dasdsa

--- src/App.js
+++ src/App.js
@@ -1,81 +1,78 @@
-import React, { Suspense } from 'react'
+import React, { Suspense } from "react";
 
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
-import Home from './pages/Home/Home'
-import Our_work from './pages/Work/Our_work';
-import About from './pages/About/About';
-import Service from './pages/service/Service';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+import Home from "./pages/Home/Home";
+import Our_work from "./pages/Work/Our_work";
+import About from "./pages/About/About";
+import Service from "./pages/service/Service";
 // import Scrollingg from './component/ScrollingText/Scrollingg.js';
-import Blog from './pages/Blog/Blog.js';
-import Influencer from './pages/Influencer/Influencer.js';
-import Our_usp from './component/Ourusp/Our_usp.jsx';
-import Whatsapp_api from './component/Whatsapp-api/Whatsapp_api.jsx';
-import Home_portfolio from './pages/Portfolio/Home_portfolio.js';
+import Blog from "./pages/Blog/Blog.js";
+import Influencer from "./pages/Influencer/Influencer.js";
+import Our_usp from "./component/Ourusp/Our_usp.jsx";
+import Whatsapp_api from "./component/Whatsapp-api/Whatsapp_api.jsx";
+import Home_portfolio from "./pages/Portfolio/Home_portfolio.js";
 
-import HomePortfolio from './pages/home-portfolio/HomePortfolio.js';
-import HomeGrid from './pages/home-grid/HomeGrid.js';
-import TestimonialSection from './pages/testimonial/TestimonialSection.js';
-import HomeAd from './pages/home-ad/HomeAd.js';
+import HomePortfolio from "./pages/home-portfolio/HomePortfolio.js";
+import HomeGrid from "./pages/home-grid/HomeGrid.js";
+import TestimonialSection from "./pages/testimonial/TestimonialSection.js";
+import HomeAd from "./pages/home-ad/HomeAd.js";
 // import Loadder from './pages/Loadder/Loadder.jsx';
-import Whatsappbutton from './pages/Whatsapp/Whatsappbutton.jsx';
-import { Helmet } from 'react-helmet';
+import Whatsappbutton from "./pages/Whatsapp/Whatsappbutton.jsx";
+import { Helmet } from "react-helmet";
 // import reelslider from './pages/Reel.js';
-import Why from './pages/Why/Why.js';
-import FaQ from './pages/FAQ/FaQ.js';
+import Why from "./pages/Why/Why.js";
+import FaQ from "./pages/FAQ/FaQ.js";
 
-import ReelSlider from './reelssection/ReelSlider.js';
-import Plans from './pages/Ourplans/Plans.js';
+import ReelSlider from "./reelssection/ReelSlider.js";
+import Plans from "./pages/Ourplans/Plans.js";
+import BehindTheSeen from "./pages/BTS/BehindTheSeen.js";
+import AnimatedVideos from "./pages/animationsection/AnimatedVideos.js";
 export default function App() {
-
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-   
     setTimeout(() => {
       setIsLoading(false);
     }, 3000); // Adjust the timeout duration as needed
   }, []);
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
+      behavior: "smooth",
     });
   });
 
   return (
     <>
+      {/* {isLoading?(<Loadder/>):( */}
 
-{/* {isLoading?(<Loadder/>):( */}
-
-   <div className='body'>
-  {/* //   <Suspense  fallback={<Loadder/>}> */}
-
-  <Home/>
-  {/* <About/> */}
-  {/* <Service/> */}
-  {/* <Scrollingg/> */}
-
-  <Why/>
-  <Our_work/>
-  <HomePortfolio/>
-  <HomeGrid/>
-  <HomeAd/>
-  <ReelSlider/>
-  <Plans/>
-  <TestimonialSection/>
-  <FaQ/>
-  {/* <Our_usp/> */}
-  <Whatsapp_api/> {/*This is collaboration / client */}
-  {/* <Influencer/> */}
-  {/* <Blog/> */}
-  
-    {/* </Suspense> */}
-  </div>
-    {/* )
+      <div className="body">
+        {/* //   <Suspense  fallback={<Loadder/>}> */}
+        <Home />
+        {/* <About/> */}
+        {/* <Service/> */}
+        {/* <Scrollingg/> */}
+        <Why />
+        <Our_work />
+        <BehindTheSeen />
+        <HomePortfolio />
+        <HomeGrid />
+        <AnimatedVideos />
+        <HomeAd />
+        <ReelSlider />
+        <Plans />
+        <TestimonialSection />
+        <FaQ />
+        {/* <Our_usp/> */}
+        <Whatsapp_api /> {/*This is collaboration / client */}
+        {/* <Influencer/> */}
+        {/* <Blog/> */}
+        {/* </Suspense> */}
+      </div>
+      {/* )
  }  */}
     </>
-    
-  )
+  );
 }
