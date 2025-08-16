68a17ddb4c04259920e7009edda60d3497a67868
ada2003 <salveadarsh23@gmail.com>
2025-08-16 21:38:06 +0530

    Test auto deployment

--- src/index.js
+++ src/index.js
@@ -1,89 +1,74 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import './media.css';
-import App from './App';
-import { BrowserRouter, Routes ,Route} from 'react-router-dom';
-import Header from './component/Header/Header';
-import Footer from './component/Footer/Footer';
-import PortfolioPage from './component/All-pages/PortfolioPage';
-import Socialmediamarketing from './component/All-pages/Allservices/Socialmediamarketing';
-import Webdevlopment from './component/All-pages/Allservices/Webdevlopment';
-import Seo from './component/All-pages/Allservices/Seo';
-import BulkWhatsapp from './component/All-pages/Allservices/BulkWhatsapp';
-import DirectMarketing from './component/All-pages/Allservices/DirectMarketing';
+import React, { Suspense } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
-import GraphicsDesigning from './component/All-pages/Allservices/GraphicsDesigning';
-import PayPerClick from './component/All-pages/Allservices/PayPerClick';
-import About_us from './pages/About_us/About_us';
-import ContactForm from './component/Contact/ContactForm';
+import Home from "./pages/Home/Home";
+import Our_work from "./pages/Work/Our_work";
+import About from "./pages/About/About";
+import Service from "./pages/service/Service";
+// import Scrollingg from './component/ScrollingText/Scrollingg.js';
+import Influencer from "./pages/Influencer/Influencer.js";
+import Our_usp from "./component/Ourusp/Our_usp.jsx";
+import Whatsapp_api from "./component/Whatsapp-api/Whatsapp_api.jsx";
+import HomePortfolio from "./pages/home-portfolio/HomePortfolio.js";
+import HomeGrid from "./pages/home-grid/HomeGrid.js";
+import Blog from "./pages/Blog/Blog.js";
 
-import Career from './pages/Career/Career';
-import InfluencersPage from './pages/Influencer/InfluencersPage';
-import CollaborationsPage from './pages/collaborations-page/CollaborationsPages';
-import Contact from './pages/Contact/Contact';
-import Service from './pages/service/Service';
-import BlogList from './pages/BlogPage-shiv/BlogList';
-import BlogPg from './pages/BlogPage-shiv/BlogPg';
-import ProductShoot from './component/All-pages/Allservices/ProductShoot';
-import Whatsappbutton from './pages/Whatsapp/Whatsappbutton';
-import ScrollTop from './ScrollTop';
-import ModelPage from './component/modelmarketing/modelPage';
-import Page404 from './component/404Page/Page404';
-import MainInfluencer from './component/Influencermodel/mainInfluencer';
-import { hydrate,render } from 'react-dom';
-import CommercialAds from './component/All-pages/Allservices/CommercialAds';
+import TestimonialSection from "./pages/testimonial/TestimonialSection.js";
+// import Loadder from './pages/Loadder/Loadder.jsx';
+import Whatsappbutton from "./pages/Whatsapp/Whatsappbutton.jsx";
+import { Helmet } from "react-helmet";
+// import reelslider from './pages/Reel.js';
+import Why from "./pages/Why/Why.js";
+import FaQ from "./pages/FAQ/FaQ.js";
+import ReelSlider from "./reelssection/ReelSlider.js";
+import BehindTheSeen from "./pages/BTS/BehindTheSeen.js";
+import AnimatedVideos from "./pages/animationsection/AnimatedVideos.js";
+import ClientTestimonial from "./pages/clienttestmonial/ClientTestimonial.jsx";
 
+export default function App() {
+  const [isLoading, setIsLoading] = React.useState(true);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
 
-// const xyz = (
-   <BrowserRouter>
-   <Header/>
-   
-    
-   
-  <Whatsappbutton/>
-    <Routes> 
-        <Route path="/" element={<App/>}/>
-        <Route path="/about" element={<About_us/>}/>
-        {/* <Route path="/portfolio" element={<PortfolioPage/>}/> */}
-        <Route path="/socialmediamarketing" element={<Socialmediamarketing/>}/>
-        <Route path="/websitedevlopment" element={<Webdevlopment/>}/>
-        <Route path="/bulkWhatsapp" element={<BulkWhatsapp/>}/>
-        <Route path="/directmarketing" element={<DirectMarketing/>}/>
-        <Route path="/model-marketing" element={<ModelPage/>}/>
-        <Route path="/model-infuencer" element={<MainInfluencer/>}/>
-        
-        
-        
-        {/* <Route path="/career" element={<Career/>}/> */}
-        <Route path="/commercialAds" element={<CommercialAds/>}/>
-        <Route path="/graphicsDesigning" element={<GraphicsDesigning/>}/>
-        <Route path="/payPerClick" element={<PayPerClick/>}/>
-        <Route path="/seo" element={<Seo/>}/>
-        <Route path="/mediaProduction" element={<ProductShoot/>}/>
-        {/* <Route path="/contact" element={<ContactForm/>}/> */}
-        <Route path="/contact" element={<Contact/>}/>
+  React.useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, []);
 
-        {/* <Route path="/blog" element={<BlogPg/>}/> */}
-        <Route path="/Influencers" element={<InfluencersPage/>}/>
-        {/* <Route path="/collaborations" element={<CollaborationsPage/>}/> */}
-        <Route path="/services" element={<Service/>}/>
-        <Route path="*" element={<Page404/>}/>
+  return (
+    <>
 
-    </Routes>
-    <ScrollTop />
-    <Footer/>
-   </BrowserRouter>
-);
+      <div className="body">
+        <Home />
+        {/* <About /> /}
+        {/ <Service /> /}
+        {/ <Scrollingg /> */}
 
-// const rootElement = document.getElementById('root');
+        <Why />
+        <Our_work />
+        <BehindTheSeen />
+        <HomePortfolio />
+        <HomeGrid />
+        <AnimatedVideos />
+        <ReelSlider />
+        <ClientTestimonial/>
+        <TestimonialSection />
+        <FaQ />
+        {/* <Our_usp /> /}
+        <Whatsapp_api />
+        {/ <Influencer /> /}
+        {/ <Blog /> /}
+      </div>
+      {/ )} */}
+    </>
+  );
+}
 
-// if(rootElement.hasChildNodes()){
-//   hydrate(xyz,rootElement);
-// }
-// else{
-//   render(xyz, rootElement);
-// }
