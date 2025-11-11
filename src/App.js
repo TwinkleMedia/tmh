37d310d3351fdd2582a03f7d4348a4ae22dd4ce6
TwinkleMedia <hs0052264@gmail.com>
2025-11-11 12:53:19 +0530

    Adarascdsasadd

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+// import React, { Suspense } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
 
