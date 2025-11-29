de8c213e8307fdf96c86444bbb00306e969e84b9
Suraaj <navdekarsuraaj3391@gmail.com>
2025-11-29 18:33:17 +0530

    services

--- src/pages/service/Service.js
+++ src/pages/service/Service.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Service.css";
 import Heading from "../../component/Heading/Heading";
 import TerminalIcon from "@mui/icons-material/Terminal";
 import ServiceCard from "../../component/ServiceCard/ServiceCard";
 import Breadcrumbs from "../../component/Breadcrumbs/Breadcrumbs";
 import { Helmet } from "react-helmet";
-
+import Allservices from "../../component/All-pages/Allservices/Mainservices/Allservices";
+import { Typewriter } from "react-simple-typewriter";
 export default function Service() {
-  React.useEffect(()=>{
+  const images = [
+    "./asset/upscaled.png",
+    "./asset/websiteMockup.png",
+  ]
+  const [currentState, setCurrentState] = useState(0)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentState((prev) => (prev + 1) % images.length)
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [images.length]);
+  React.useEffect(() => {
     window.scrollTo({
       top: 0,
       behavior: 'smooth'
@@ -46,16 +58,66 @@ export default function Service() {
         <meta property="og:type" content="website" />
       </Helmet>
       <Breadcrumbs breadheading={"Services"} breadcrumbsLabel={"Services"} />
+      <div className="smm-hero-section">
+        {/* <Breadcrumbs
+                breadheading={"Social Media (SMM)"}
+                breadcrumbsLabel={"Social Media (SMM)"}
+              /> */}
+        <section className="hero-section">
+          <div className="hero-content">
+            <Allservices
+              Serviceh1={<>
+                Services We Provide <br />
+              </>}
+              Serviceh1span={<span>
+                <Typewriter
+                  words={["Social Media Marketing", "Commercial Ads..", "Web Development..", "SEO..", "Graphic Design..", "Media Production..", "Pay Per Click", "And Many More.."]}
+                  loop={0}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={100}
+                  deleteSpeed={50}
+                  delaySpeed={2000} />
+              </span>}
+            // h4para={""}
+            />
+            {/* <h1>
+                    Social Media Marketing <br /> Agency In Navi Mumbai
+                  </h1> */}
+
+            <p className="subtitle">
+              Partner with Twinkle Media Hub Pvt Ltd and elevate your digital presence today.
+            </p>
+
+            <p className="description">
+              As a premier social media marketing agency, we craft impactful digital
+              strategies that boost engagement and drive conversions. Our mission is
+              to transform your brand’s presence with measurable results.
+            </p>
+
+            <button className="hero-btn">Get Started</button>
+          </div>
+
+          <div className="hero-image hero-image-slider">
+            <img key={currentState} src={images[currentState]} alt="images Slider" className="slider-Image fadeSlide" />
+          </div>
+        </section>
+        {/* <Allservices
+                // Serviceh1={"Social Media Marketing Agency"}
+                // Serviceh1span={"In Navi Mumbai"}
+                h4para={""}
+              /> */}
+      </div>
       <Heading headingLabel="Best Digital Marketing Agency in Mumbai" />
       <div className="container">
         <div className="row">
 
-              <ServiceCard
+          <ServiceCard
             url="CommercialAds"
             icon={<TerminalIcon />}
             heading="Commercial Ads"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
 
           <ServiceCard
@@ -63,7 +125,7 @@ export default function Service() {
             icon={<TerminalIcon />}
             heading="SOCIAL MEDIA MARKETING (SMM)"
             className="test textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
 
           <ServiceCard
@@ -71,7 +133,7 @@ export default function Service() {
             icon={<TerminalIcon />}
             heading="WEB DEVELOPMENT"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
 
           <ServiceCard
@@ -79,29 +141,29 @@ export default function Service() {
             icon={<TerminalIcon />}
             heading="SEO (SEARCH ENGINE OPTIMIZATION)"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
           <ServiceCard
             url="GraphicsDesigning"
             icon={<TerminalIcon />}
             heading="GRAPHIC DESIGN"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
-      
+
           <ServiceCard
             url="bulkWhatsapp"
             icon={<TerminalIcon />}
             heading="BULK WHATASPP MARKETING"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
           <ServiceCard
             url="Directmarketing"
             icon={<TerminalIcon />}
             heading="DIRECT MARKETING"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
           <ServiceCard
             url="PayPerClick"
@@ -109,14 +171,14 @@ export default function Service() {
             heading="PAY PER CLICK"
             className="textColor"
           >
-            <br/><br/>
+            <br /><br />
           </ServiceCard>
           <ServiceCard
             url="mediaProduction"
             icon={<TerminalIcon />}
             heading="Media Production"
             className="textColor"
-          ><br/><br/>
+          ><br /><br />
           </ServiceCard>
         </div>
       </div>
