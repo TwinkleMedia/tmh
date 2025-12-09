d6078b83a8dbe6aab9dc5090598b000d62797881
prernaingle009 <prernaingle090@gmail.com>
2025-12-09 18:37:01 +0530

    teamSection

--- src/component/All-pages/Allservices/Team.jsx
+++ src/component/All-pages/Allservices/Team.jsx
@@ -89,15 +89,38 @@ export default function Team() {
     `}
         </script>
       </Helmet>
+<section className="leaders-section">
 
+                {/* Left Side - CEO */}
+                <div className="leader left">
+                  <div className="leader-content">
+                    <div className="leader-img">
+                      <img src="./asset/Team/maam.png" alt="CEO" />
+                    </div>
+                    <div className="leader-text">
+                      <h2 className="leader-name">Deepika Rajan</h2>
+                      <p className="leader-role">-CEO</p>
+                    </div>
+                  </div>
+                </div>
+
+                {/* Right Side - Managing Director */}
+                <div className="leader right">
+                  <div className="leader-content">
+                    <div className="leader-text right-text">
+                      <h2 className="leader-name">Harpinder Singh</h2>
+                      <p className="leader-role">-Managing Director</p>
+                    </div>
+                    <div className="leader-img">
+                      <img src="./asset/Team/sir.png" alt="managing director" />
+                    </div>
+                  </div>
+                </div>
+
+              </section>
       <div className="container">
         <div className="leadership-section">
           <div className="container">
-            <div className="header-content">
-              <div className="brand-line"></div>
-              <h1 className="Teammain-title">Twinke Media Hub</h1>
-              {/* <p className="teamsub textCenter">Guiding Twinkle Media Hub to Excellence</p> */}
-            </div>
 
             {/* <div className="leaders-container">
               <div className="leader-card">
